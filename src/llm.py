6087904f16e173db1ab25d80f7149d9e08b02124
AndreaFilippini <56972407+AndreaFilippini@users.noreply.github.com>
2025-03-21 20:54:51 +0100

    added requirements

--- src/llm.py
+++ src/llm.py
@@ -1,23 +1,22 @@
 import os
 import sys
 import json
+import requests
 import pandas as pd
 import numpy as np
+from functools import partial
 from dotenv import load_dotenv, find_dotenv
 from huggingface_hub import login
 from huggingface_hub import InferenceClient
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.schema.runnable import RunnableSequence
 from sentence_transformers import SentenceTransformer
-import requests
-from langchain_huggingface import HuggingFaceEmbeddings
 from llama_index.core import VectorStoreIndex, Settings
 from llama_index.core.schema import TextNode
 from llama_index.core.storage.storage_context import StorageContext
 from llama_index.core.vector_stores import SimpleVectorStore
 import gradio as gr
-
-transformer_path = "sentence-transformers/all-MiniLM-L6-v2"
 
 # number of movies to be retrieved
 total_movies = 50
@@ -47,13 +46,13 @@ def format_user_preferences(preferences):
     return "\n".join(formatted_preferences)
 
 # function used to do a genre mapping, by relating the id to the gender name
-def fetch_genre_mapping(api_key):
+def fetch_genre_mapping(tmdb_token):
     url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={tmdb_token}&language=en-US"
     response = requests.get(url).json()
     return {genre['id']: genre['name'] for genre in response['genres']}
 
 # function used to fetch movie data
-def fetch_movie_data(api_key, num_movies):
+def fetch_movie_data(tmdb_token, num_movies):
     url = f"https://api.themoviedb.org/3/movie/popular?api_key={tmdb_token}&language=en-US&page=1"
     response = requests.get(url).json()
     return response['results'][:num_movies]
@@ -77,19 +76,23 @@ def preprocess_movie_data(movies, genre_mapping):
     return processed_movies
 
 # function used to interface with llm to get advice from movies
-def get_llm_response(user_input, user_id):
+def get_llm_response(hugging_token, tmdb_token, user_input, user_id):
+    # login to huggingface
+    login(token = hugging_token)
+   
     # get the user's preferences and format them
     preferences = load_user_preferences(user_id, "preferences.json")
     formatted_preferences = format_user_preferences(preferences)
 
     # get the movies information from TMDB APIs and format it
     genre_mapping = fetch_genre_mapping(tmdb_token)
-    movies = fetch_movie_data(tmdb_token, num_movies=total_movies)
+    movies = fetch_movie_data(tmdb_token, total_movies)
     processed_movies = preprocess_movie_data(movies, genre_mapping)
     
     # init embedding model
+    transformer_path = "sentence-transformers/all-MiniLM-L6-v2"
     embedding_model = SentenceTransformer(transformer_path)
-    Settings.embed_model = HuggingFaceEmbeddings(model_name=transformer_path)
+    Settings.embed_model = HuggingFaceEmbeddings(model_name = transformer_path)
     
     # Generate embeddings for movie data
     movie_embeddings = []
@@ -102,8 +105,8 @@ def get_llm_response(user_input, user_id):
     nodes = []
     for movie, (movie_id, embedding) in zip(processed_movies, movie_embeddings):
         node = TextNode(
-            text=f"{movie['title']} {movie['description']} {movie['genres']}",
-            embedding=embedding.tolist(),
+            text = f"{movie['title']} {movie['description']} {movie['genres']}",
+            embedding = embedding.tolist(),
             metadata={
                 "id": movie['id'],
                 "title": movie['title'],
@@ -117,10 +120,10 @@ def get_llm_response(user_input, user_id):
     vector_store = SimpleVectorStore()
 
     # Create a storage context
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    storage_context = StorageContext.from_defaults(vector_store = vector_store)
 
     # Create an index
-    index = VectorStoreIndex(nodes=nodes, storage_context=storage_context)
+    index = VectorStoreIndex(nodes = nodes, storage_context=storage_context)
 
     # uses llama index to get the five similar movies with respect to the input
     retriever = index.as_retriever(similarity_top_k = 5)
@@ -134,15 +137,15 @@ def get_llm_response(user_input, user_id):
     )
 
     # format the prompt
-    prompt = prompt_template.format(user_input=user_input, user_preferences=formatted_preferences, retrieved_text=retrieved_text)
+    prompt = prompt_template.format(user_input = user_input, user_preferences = formatted_preferences, retrieved_text = retrieved_text)
 
     # init client for making requests to HuggingFace Inference API
     client = InferenceClient(token=hugging_token)
     
     # query the model to get movie suggestions
     response = client.chat_completion(
-        model="mistralai/Mistral-7B-Instruct-v0.2",
-        messages=[{"role": "user", "content": prompt}]
+        model = "mistralai/Mistral-7B-Instruct-v0.2",
+        messages = [{"role": "user", "content": prompt}]
     )
     
     return response["choices"][0]["message"]["content"]
@@ -152,35 +155,25 @@ if __name__ == "__main__":
     load_dotenv(find_dotenv())
     
     # checks that all tokens are available
-    hugging_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-    tmdb_token = os.getenv("TMDB_API_KEY")
-    openai_token = os.getenv("OPENAI_API_KEY")
+    token_list = ["HUGGINGFACEHUB_API_TOKEN", "TMDB_API_KEY", "OPENAI_API_KEY"]
+    for token in token_list:
+        if not os.getenv(token):
+            print(f"ERROR: {token} not found\n")
+            sys.exit()
     
-    if not hugging_token:
-        print("ERRORE: HUGGING FACE token not found\n")
-        sys.exit()
-
-    if not tmdb_token:
-        print("ERRORE: TMDB token not found\n")
-        sys.exit()
-    
-    if not openai_token:
-        print("ERRORE: OPENAI token not found\n")
-        sys.exit()
-
-    # login to huggingface
-    login(token=hugging_token) 
+    # statically add tokens arguments to query with the llm
+    llm_function = partial(get_llm_response, os.getenv("HUGGINGFACEHUB_API_TOKEN"), os.getenv("TMDB_API_KEY"))
    
     # use gradio for creating a simple graphical interface with which to interact with the llm
     iface = gr.Interface(
-        fn = get_llm_response,
-        inputs=[
+        fn = llm_function,
+        inputs = [
             gr.Textbox(label="Enter your prompt"),
-            gr.Textbox(label="Enter your user ID")
+            gr.Textbox(label="Enter your user ID"),
         ],
-        outputs=gr.Textbox(label="LLM Response"),
-        flagging_mode="never",
-        title="Movie Recommendation System",
-        description="Enter a prompt and your user ID to get personalized movie recommendations."
+        outputs = gr.Textbox(label = "LLM Response"),
+        flagging_mode = "never",
+        title = "Movie Recommendation System",
+        description = "Enter a prompt and your user ID to get personalized movie recommendations."
     )
     iface.launch()
